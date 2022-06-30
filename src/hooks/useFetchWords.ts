4af4f8822ec5c52db923916eb78b3a5e34a7c409
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2022-06-30 18:04:55 +0300

    fetchHook

--- src/hooks/useFetchWords.ts
+++ src/hooks/useFetchWords.ts
@@ -1,9 +1,33 @@
 import React from 'react'
+import axios from 'axios'
 
 const useFetchWords = () => {
-  const [words, setWords] = React.useState([])
+  const [word, setWord] = React.useState('')
+  const [error, setError] = React.useState('')
 
-  return
+  console.log({ word })
+
+  React.useEffect(() => {
+    const getWords = async () => {
+      try {
+        const word = await axios.get(
+          'https://wordsapiv1.p.rapidapi.com/words/?letters=5&random=true',
+          {
+            headers: {
+              'X-Mashape-Key':
+                'b1712aea8cmsh61933a383dfa48dp1e3d0bjsn49e2c2cdbe3b',
+            },
+          },
+        )
+        setWord(word.data.word)
+      } catch (error) {
+        setError('Error occurred while fetching words')
+      }
+    }
+    getWords()
+  }, [])
+
+  return [word, error]
 }
 
 export default useFetchWords
